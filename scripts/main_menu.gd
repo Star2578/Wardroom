bbf1702fa2f37d626594a9d999adaf0625cb08cb
Star2578 <nuttykingger@gmail.com>
2026-02-02 16:15:57 +0700

    create cs_01 for game intro

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,8 +1,16 @@
 extends Control
 
+@export var animation : AnimationPlayer
+
+@onready var start_btn = $Start
+@onready var option_btn = $Option
+@onready var quit_btn = $Quit
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	animation.play("cs_01")
+	start_btn.disabled = true
+	option_btn.disabled = true
+	quit_btn.disabled = true
 
 
 func _on_option_pressed() -> void:
