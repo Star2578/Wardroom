710da6f4b0deb8adfe287ec9b9559e86293facb6
Star2578 <nuttykingger@gmail.com>
2026-02-03 10:40:16 +0700

    adjust CutscenePlayer to change TextWriter cs_code when scene change

--- scripts/cutscene_player.gd
+++ scripts/cutscene_player.gd
@@ -3,13 +3,22 @@ extends Node
 
 @export var shots : Array[String]
 @export var anim : AnimationPlayer
+@export var text_writer : TextWriter
 
 var current_index = 0
+var current_shot : String
+
+func _ready() -> void:
+	text_writer.set_dialogue_cs(shots[0])
 
 func next_shot():
 	if current_index > shots.size():
 		print("Cutscene Finished")
 		return
 	
-	anim.play(shots[current_index])
+	current_shot = shots[current_index]
+	
+	anim.play(current_shot)
 	current_index += 1
+	text_writer.set_dialogue_cs(current_shot)
+	text_writer.current_index = 0
--- scripts/text_writer.gd
+++ scripts/text_writer.gd
@@ -1,11 +1,13 @@
 extends Control
+class_name TextWriter
 
 @export var json_path: String
 
 @onready var rich_label = $RichTextLabel
 
-var dialogue_data: Array = []
+var dialogue_data: Dictionary = {}
 var current_index = 0
+var current_cs = ""
 var typing_speed = 0.05 # Seconds per character
 
 func _ready():
@@ -19,11 +21,14 @@ func load_dialogue_from_json(file_path: String):
 			var content = file.get_as_text()
 			var parse_result = JSON.parse_string(content)
 			if parse_result is Dictionary:
-				dialogue_data = parse_result["text"]
+				dialogue_data = parse_result
 				print(dialogue_data)
 				print(dialogue_data.size())
 		file.close()
 	return dialogue_data
+
+func set_dialogue_cs(cs_code: String):
+	current_cs = cs_code
 
 func show_next_dialogue():
 	if current_index >= dialogue_data.size():
@@ -31,7 +36,7 @@ func show_next_dialogue():
 		return
 
 	# 1. Set the text
-	rich_label.text = dialogue_data[current_index]
+	rich_label.text = dialogue_data[current_cs][current_index]
 	current_index += 1
 	
 	# 2. Reset visibility and animate
