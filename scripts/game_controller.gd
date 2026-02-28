1035e35530a229a0ce07064850d737e9a37865b5
go <poompatgogo@gmail.com>
2026-03-01 00:58:50 +0700

    Add soundcheck audio feature and enhance volume control functionality

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -4,21 +4,22 @@ extends Node
 var is_started: bool = false
 
 var mouse_sensitivity: float = 0.1
-var master_volume_db: float = 0.0
+var master_bus_index: int
+var master_volume_db: float = linear_to_db(0.5)
 var master_muted: bool = false
 var brightness: float = 1.0
 
 func _ready() -> void:
 	print("GameManager Initiated")
-	master_volume_db = AudioServer.get_bus_volume_db(0)
-	master_muted = AudioServer.is_bus_mute(0)
+	master_bus_index = AudioServer.get_bus_index("Master")
+	master_muted = AudioServer.is_bus_mute(master_bus_index)
 	apply_audio_settings()
 	apply_brightness_settings()
 
 
 func apply_audio_settings() -> void:
-	AudioServer.set_bus_volume_db(0, master_volume_db)
-	AudioServer.set_bus_mute(0, master_muted)
+	AudioServer.set_bus_volume_db(master_bus_index, master_volume_db)
+	AudioServer.set_bus_mute(master_bus_index, master_muted)
 
 func apply_brightness_settings() -> void:
 	for node in get_tree().get_nodes_in_group("world_environments"):
--- scripts/option.gd
+++ scripts/option.gd
@@ -1,9 +1,14 @@
 extends Control
 
+# soundcheck.mp3
+const SOUNDCHECK_PATH := "uid://yfrcsc1kgiqr"
+
 @onready var language_option: OptionButton = $MarginContainer/VBoxContainer/VolumeRow2/OptionButton
 @onready var mute_checkbox: CheckBox = $MarginContainer/VBoxContainer/VolumeRow/Mute
 @onready var volume_slider: HSlider = $MarginContainer/VBoxContainer/VolumeRow/Volume
 @onready var brightness_slider: HSlider = $MarginContainer/VBoxContainer/VolumeRow3/Brightness
+
+var soundcheck_player: AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +16,9 @@ func _ready() -> void:
 		language_option.add_item("English")
 		language_option.add_item("Thai")
 	language_option.select(0)
-	volume_slider.value = GameController.master_volume_db
+	_setup_soundcheck_player()
+
+	volume_slider.value = db_to_linear(GameController.master_volume_db)
 	mute_checkbox.button_pressed = GameController.master_muted
 	GameController.apply_audio_settings()
 
@@ -22,12 +29,24 @@ func _process(delta: float) -> void:
 
 
 func _on_volume_value_changed(value: float) -> void:
-	GameController.master_volume_db = value
+	if value <= 0.0:
+		GameController.master_volume_db = -80.0
+	else:
+		GameController.master_volume_db = linear_to_db(value)
 	GameController.apply_audio_settings()
 
 func _on_mute_button_toggled(toggled_on: bool) -> void:
 	GameController.master_muted = toggled_on
 	GameController.apply_audio_settings()
+
+func _on_volume_drag_ended(value_changed: bool) -> void:
+	if soundcheck_player and soundcheck_player.stream:
+		soundcheck_player.play()
+
+func _setup_soundcheck_player() -> void:
+	soundcheck_player = AudioStreamPlayer.new()
+	soundcheck_player.stream = load(SOUNDCHECK_PATH) as AudioStream
+	add_child(soundcheck_player)
 
 func _on_brightness_value_changed(value: float) -> void:
 	GameController.brightness = value
